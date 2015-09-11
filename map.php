0a389ec43767d5819992d81645465811e0e006d2
pete101011 <lee1509@purdue.edu>
2015-09-11 01:01:14 -0400

    Initial file

---
+++ map.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Basic Page Needs
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta charset="utf-8">
+    <title>Concept Map</title>
+    <meta name="description" content="Canvas Map">
+    <meta name="author" content="Purdue Cognition and Learning Lab">
+
+    <!-- Mobile Specific Metas
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+
+    <!-- Scripts
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <!--<script src="js/main.js"></script>-->
+
+  </head>
+  <body>
+
+    <script type="application/javascript">
+    </script>
+  </body>
+</html>
