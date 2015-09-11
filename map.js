87840f3776577167938e9df45a43c65c08d35d68
Matt Molo <molo.matt@gmail.com>
2015-09-11 01:16:23 -0400

    Initial merging of code to one page

--- map.js
+++ map.js
@@ -7,80 +7,117 @@ var editing = false;
 
 var mousedown = false;
 
-canvasElem.addEventListener("mousedown", function(event) {
-  mouseDown = true;
-  
-  var x = Math.round(event.clientX - canvasElem.getBoundingClientRect().left);
-  var y = Math.round(event.clientY - canvasElem.getBoundingClientRect().top);
-  
-  console.log(x, y);
-  
-  var inNode = insideNode(x, y);
+var canvasElem = document.getElementById("canvas");
+var canvas = canvasElem.getContext("2d");
+canvas.imageSmoothingEnabled = false;
 
-  if (inNode < 0) {
-    var node = new rectangle(x, y, 60, 25, 'blue');
-    node.draw();
-    nodes.push(node);
-  } else {
-    startNode = inNode;
-  }
-   
-  
-}, false);
+var wordElem = $(".wordElem");
+		
 
-canvasElem.addEventListener("mouseup", function(event) {
-  mouseDown = false;
-  var x = Math.round(event.clientX - canvasElem.getBoundingClientRect().left);
-  var y = Math.round(event.clientY - canvasElem.getBoundingClientRect().top);
-  
-  console.log(x, y);
-  
-  var inNode = insideNode(x, y);
-  
-  if (inNode == startNode) {
-    var node = nodes[inNode];
-    wordElem.css("width", node.width + "px");
-    wordElem.css("left", node.x - (node.width/2)+"px");
-    wordElem.css("top",  node.y + (node.height/2)+"px");
-    wordElem.show();
-  } else if (inNode >= 0 && startNode >= 0) {
-    var connection = [startNode, inNode];
-    connections.push(connection);
-    drawLine(nodes[startNode].x, nodes[startNode].y, nodes[inNode].x, nodes[inNode].y, 'black');
-    startNode = -1;
-  }
-   
-  
-}, false);
+function init() {
+	canvasElem.addEventListener("mousedown", function(event) {
+		mouseDown = true;
+		
+		var x = Math.round(event.clientX - canvasElem.getBoundingClientRect().left);
+		var y = Math.round(event.clientY - canvasElem.getBoundingClientRect().top);
+		
+		var inNode = insideNode(x, y);
 
+		if (inNode < 0) {
+			var node = new rectangle(x, y, 60, 25, 'blue');
+			node.draw();
+			nodes.push(node);
+		} else {
+			startNode = inNode;
+		}
+	}, false);
+
+	canvasElem.addEventListener("mouseup", function(event) {
+		mouseDown = false;
+		var x = Math.round(event.clientX - canvasElem.getBoundingClientRect().left);
+		var y = Math.round(event.clientY - canvasElem.getBoundingClientRect().top);
+		
+		console.log(x, y);
+		
+		var inNode = insideNode(x, y);
+		
+		if (inNode == startNode) {
+			var node = nodes[inNode];
+			wordElem.css("width", node.width + "px");
+			wordElem.css("left", node.x - (node.width/2)+"px");
+			wordElem.css("top",  node.y + (node.height/2)+"px");
+			wordElem.show();
+		} else if (inNode >= 0 && startNode >= 0) {
+			var connection = [startNode, inNode];
+			connections.push(connection);
+			drawLine(nodes[startNode].x, nodes[startNode].y, nodes[inNode].x, nodes[inNode].y, 'black');
+			startNode = -1;
+		}
+	}, false);
+}
 
 function insideNode(x, y) {
-  for (var i = 0; i < nodes.length; i++) {
-    if (x < nodes[i].x + nodes[i].width/2  && x > nodes[i].x - nodes[i].width/2 && y < nodes[i].y + nodes[i].height/2 && y > nodes[i].y - nodes[i].height/2) {
-      console.log("Inside node", i);
-      return i;
-    }
-  }
-  
-  return -1;
+	for (var i = 0; i < nodes.length; i++) {
+		if (x < nodes[i].x + nodes[i].width/2  && x > nodes[i].x - nodes[i].width/2 && 
+			y < nodes[i].y + nodes[i].height/2 && y > nodes[i].y - nodes[i].height/2) {
+			console.log("Inside node", i);
+			return i;
+		}
+	}
+	
+	return -1;
+}
+	
+function drawLine(x, y, x2, y2, color) {
+	canvas.beginPath();
+	canvas.moveTo(x, y);
+	canvas.lineTo(x2, y2);
+	canvas.closePath();
+	canvas.strokeStyle = color;
+	canvas.stroke();
 }
 
-function resize() {
-  while(wordElem.height() > 25) {
-    wordElem.css("width", (wordElem.width() + 50) + "px");
-  }
+
+function rectangle(x, y, width, height, color) {
+	this.x = x;
+	this.y = y;
+	this.deltaX = 0;
+	this.deltaY = 0;
+	this.angle = 0;
+	this.deltaAngle = 0;
+	
+	this.width = width;
+	this.height = height;
+	this.deltaWidth = 0;
+	this.deltaHeight = 0;
+	
+	this.color = color;
+	
+	this.radAngle = function() {
+		return (this.angle*Math.PI)/180;
+	}
+
+	this.draw = function() {
+		canvas.save();
+		canvas.translate(this.x, this.y);
+		canvas.rotate(this.radAngle());
+		
+		canvas.fillStyle = color;
+		canvas.fillRect(-(this.width/2), -(this.height/2), this.width, this.height);
+		
+		canvas.restore();
+	}
 }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+function clearCanvas() {
+	canvas.clearRect(0 , 0 , canvasElem.width, canvasElem.height);
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
--- map.php
+++ map.php
@@ -1,31 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Basic Page Needs
-    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-    <meta charset="utf-8">
-    <title>Concept Map</title>
-    <meta name="description" content="Canvas Map">
-    <meta name="author" content="Purdue Cognition and Learning Lab">
+	<head>
+		<!-- Basic Page Needs
+		–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+		<meta charset="utf-8">
+		<title>Concept Map</title>
+		<meta name="description" content="Canvas Map">
+		<meta name="author" content="Purdue Cognition and Learning Lab">
 
-    <!-- Mobile Specific Metas
-    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- Mobile Specific Metas
+		–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- CSS
-    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/main.css">
+		<!-- CSS
+		–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="css/main.css">
 
-    <!-- Scripts
-    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <!--<script src="js/main.js"></script>-->
+		<!-- Scripts
+		–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+		
+		<!--<script src="js/main.js"></script>-->
 
-  </head>
-  <body>
+	</head>
+	<body>
 
-    <script type="application/javascript">
-    </script>
-  </body>
+		<div id="center">
+			<canvas width="800" height="600" id="canvas"></canvas>
+		<div contenteditable="true" class="wordElem" onkeyup="resize()" onkeydown="resize()"></div>
+
+		<script type="application/javascript">
+		</script>
+		<script src="map.js"></script>
+
+	</body>
 </html>
