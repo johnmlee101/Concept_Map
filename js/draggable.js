4218b4bda16f42da6c91950fc6e31b9875ea622f
pete101011 <lee1509@purdue.edu>
2015-10-30 02:38:30 -0400

    1 time word bank

--- js/draggable.js
+++ js/draggable.js
@@ -10,6 +10,9 @@ function loadWordList(index) {
 	wordList.forEach(function(word) {
 		$("#canvas2").append('<div class="draggable">' + word + "&nbsp;</div>");
 	});
+	$(".draggable").each(function(element) {
+		$(this).css("font-weight", "bold");
+	});
 	$(".draggable").draggable({
 		revert: true,
 		revertDuration: 0,
@@ -21,6 +24,9 @@ function loadWordList(index) {
 			var height = $(".canvas-holder").height();
 			if(ui.offset.left > offset.left && ui.offset.left < offset.left + width && ui.offset.top > offset.top && ui.offset.top < offset.top + height) {
 				conceptMap.addNode(new Node(ui.offset.left - offset.left, ui.offset.top - offset.top, this.innerText.split(" ")[0].replace(/[.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"")));
+				$(this).draggable("disable");
+				$(this).css("cursor", "default");
+				$(this).css("font-weight", "300");
 			}
 		}
 	});
